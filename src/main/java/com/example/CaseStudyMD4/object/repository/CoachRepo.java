e7879ae9533d3b50be03b08a44fb8a7fe44dc34d
Thangggg <thang.nguyenvan7594@gmail.com>
2022-02-07 11:56:38 +0700

    test

---
+++ src/main/java/com/example/CaseStudyMD4/object/repository/CoachRepo.java
@@ -0,0 +1,7 @@
+package com.example.CaseStudyMD4.object.repository;
+
+import com.example.CaseStudyMD4.object.model.Coach;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CoachRepo extends CrudRepository<Coach,Long> {
+}
