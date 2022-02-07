a0d03cc75ff70fce61c98fe81d7dbbe71f06bc32
thao <vuthao0410@gmail.com>
2022-02-07 11:00:47 +0700

    coach model changes

--- src/main/java/com/example/CaseStudyMD4/object/model/Coach.java
+++ src/main/java/com/example/CaseStudyMD4/object/model/Coach.java
@@ -13,6 +13,9 @@ public class Coach {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String description;
+    private String email;
+    private String phone;
 
     @OneToMany
     private List<Class> classList;
